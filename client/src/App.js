a7fe86db4251276a2e5509d6d80b56ef06b157ec
xtreme6007 <prestonnichols96@gmail.com>
2020-12-22 09:48:17 -0600

    deploying

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Restaurants from "./pages/restaurants";
-import Detail from "./pages/Detail";
+
 import Randomizer from "./pages/Randomizer";
 import Landing from "./pages/Landing";
 import './pages/RandomStyle.css'
@@ -22,9 +22,7 @@ function App() {
           <Route exact path={["/", "/addRestaurant"]}>
             <Restaurants />
           </Route>
-          <Route exact path="/restaurants/:id">
-            <Detail />
-          </Route>
+          
           <Route exact path={["/","/randomizer"]}>
             <Randomizer />
           </Route>
